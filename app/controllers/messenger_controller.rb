04f09620b91c0f63ccb6c9dffa6a9c70cb9ac17b
Tomer Mintz <tomer.mintz@gmail.com>
2016-07-27 06:23:16 +0300

    add gem messenger-ruby

---
+++ app/controllers/messenger_controller.rb
@@ -0,0 +1,6 @@
+class MessengerController < Messenger::MessengerController
+  def webhook
+    #logic here
+    render nothing: true, status: 200
+  end
+end
---
+++ config/initializers/messenger.rb
@@ -0,0 +1,4 @@
+Messenger.configure do |config|
+  config.verify_token      = 'lital14' #will be used in webhook verifiction
+  config.page_access_token = 'EAASUFjpCiM4BAPmYnciPHL6aORBV7kfDZADB9SpYauwUsNBg2w7xxaAHLSQnecHFf8rumhZBRxODZALC7pJZBru3gZCDENSDerTVCr7pyoMVtnpXwshn1kYSNA6UhrrxW1CyOm6GYNuBRBwuHOKOVgV3besdsRu3EHAIi4xZCwywZDZD'
+end
