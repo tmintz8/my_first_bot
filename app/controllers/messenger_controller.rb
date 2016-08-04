f3896414f7133a9ec80a9784b7f61a95e060e7d1
Tomer Mintz <tomer.mintz@gmail.com>
2016-08-04 13:56:57 +0300

    First bot answer

--- app/controllers/messenger_controller.rb
+++ app/controllers/messenger_controller.rb
@@ -1,6 +1,19 @@
 class MessengerController < Messenger::MessengerController
   def webhook
-    #logic here
+
+	logger.info "Processing the request..."
+	if fb_params.first_entry.callback.message?
+	  Messenger::Client.send(
+	    Messenger::Request.new(
+	      Messenger::Elements::Text.new(text: 'some text02'),
+	      fb_params.first_entry.sender_id
+	    )
+
+	   Messenger::MessengerController::req
+	  )
+	end
+
+	#make sure to send 200 at the end
     render nothing: true, status: 200
   end
 end
